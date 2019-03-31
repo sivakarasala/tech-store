4389d42555283834c6f4ed7281f5a3c37e812cec
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-31 19:33:07 -0400

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
 
 import Home from "./pages/HomePage";
 import About from "./pages/AboutPage";
@@ -12,7 +13,20 @@ import Default from "./pages/Default";
 
 class App extends Component {
   render() {
-    return <h1>Aum Namah Shivaya from tech store</h1>;
+    return (
+      <React.Fragment>
+        {/* navbar, sidebar, cart, footer */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/products/:id" component={SingleProduct} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
+    );
   }
 }
 
