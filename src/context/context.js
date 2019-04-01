35972288b729863ff350f3f1c7fc6f43925a057d
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-31 20:14:13 -0400

    navbar context api

--- src/context/context.js
+++ src/context/context.js
@@ -4,9 +4,43 @@ const ProductContext = React.createContext();
 //Provider
 //Consumer
 class ProductProvider extends Component {
+  state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    cartItems: 0
+  };
+
+  // handle sidebar
+  handleSidebar = () => {
+    this.setState({ sidebarOpen: !this.state.sidebarOpen });
+  };
+
+  // handle cart
+  handleCart = () => {
+    this.setState({ cartOpen: !this.state.cartOpen });
+  };
+
+  // close cart
+  closeCart = () => {
+    this.setState({ cartOpen: false });
+  };
+
+  // open cart
+  openCart = () => {
+    this.setState({ cartOpen: true });
+  };
+
   render() {
     return (
-      <ProductContext.Provider value="aum namah shivaya from context">
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleSidebar: this.handleSidebar,
+          handleCart: this.handleCart,
+          openCart: this.openCart,
+          closeCart: this.closeCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,12 @@ import { ProductConsumer } from "../context";
 export default function HomePage() {
   return (
     <React.Fragment>
-      <ProductConsumer>{value => <h1>{value}</h1>}</ProductConsumer>
+      <ProductConsumer>
+        {value => {
+          console.log(value);
+          return <h1>Aum Namah Shivaya from home page</h1>;
+        }}
+      </ProductConsumer>
     </React.Fragment>
   );
 }
