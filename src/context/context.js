5e0a8db2ef1dc6eacf1c73630534e3051cbe0271
shambho <Sivakrishna.Karasala@eku.edu>
2019-04-01 20:38:08 -0400

    add footer

--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { linkData } from "./linkData";
-
+import { socialData } from "./socialData";
 const ProductContext = React.createContext();
 //Provider
 //Consumer
@@ -10,6 +10,7 @@ class ProductProvider extends Component {
     cartOpen: false,
     cartItems: 0,
     links: linkData,
+    socialIcons: socialData,
     cart: []
   };
 
