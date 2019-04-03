9a62d189c2e34a13e8f3f0e65b5bb903f8433186
shambho <Sivakrishna.Karasala@eku.edu>
2019-04-02 22:10:26 -0400

    add singleProduct page setup

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,9 +1,74 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import singleProductImg from "../images/singleProductBcg.jpeg";
+import { ProductConsumer } from "../context";
 
 export default function SingleProductPage() {
   return (
     <React.Fragment>
-      <h1>Aum namah shivaya from single product page</h1>
+      <Hero img={singleProductImg} title="single product" />
+      <ProductConsumer>
+        {value => {
+          const { singleProduct, addToCart, loading } = value;
+          if (loading) {
+            console.log("aum namah shivaya from loading");
+            return <h1>aum namah shivaya...</h1>;
+          }
+          const {
+            company,
+            description,
+            id,
+            price,
+            title,
+            image
+          } = singleProduct;
+          return (
+            <section className="py-5">
+              <div className="container">
+                <div className="row">
+                  <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
+                    <img
+                      src={`../${image}`}
+                      // src={image} when using with contentful
+                      alt="single product"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
+                    <h5 className="text-title mb-4">model :{title}</h5>
+                    <h5 className="text-capitalize text-muted mb-4">
+                      company : {company}
+                    </h5>
+                    <h5 className="text-main text-capitalize mb-4">
+                      price : ${price}
+                    </h5>
+                    <p className="text-capitalize text-title mt-3">
+                      product info :
+                    </p>
+                    <p> {description}</p>
+                    <button
+                      type="button"
+                      className="main-link"
+                      style={{ margin: "0.75rem" }}
+                      onClick={() => addToCart(id)}
+                    >
+                      add to cart
+                    </button>
+                    <Link
+                      to="/products"
+                      className="main-link"
+                      style={{ margin: "0.75rem" }}
+                    >
+                      back to products
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </section>
+          );
+        }}
+      </ProductConsumer>
     </React.Fragment>
   );
 }
