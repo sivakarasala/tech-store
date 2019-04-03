618e08304fba926e3187de494045fbb47c8d9e80
shambho <Sivakrishna.Karasala@eku.edu>
2019-04-02 22:25:26 -0400

    add side cart component

--- src/context/context.js
+++ src/context/context.js
@@ -8,7 +8,7 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
   state = {
     sidebarOpen: false,
-    cartOpen: false,
+    cartOpen: true,
     links: linkData,
     socialIcons: socialData,
     cart: [],
