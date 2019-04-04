f7c6a65ed85f2bb85db55aeb440c5c6d3b58fb79
shambho <Sivakrishna.Karasala@eku.edu>
2019-04-04 14:38:37 -0400

    add handleChange method logic

--- src/context/context.js
+++ src/context/context.js
@@ -236,7 +236,17 @@ class ProductProvider extends Component {
 
   // handle filtering
   handleChange = event => {
-    console.log("aum namah shivaya", event);
+    const name = event.target.name;
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    this.setState(
+      {
+        [name]: value
+      },
+      this.sortData
+    );
   };
 
   sortData = () => {};
