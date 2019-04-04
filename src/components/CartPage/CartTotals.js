ccca2336baecfcb7c61afc7865e53474651c398b
shambho <Sivakrishna.Karasala@eku.edu>
2019-04-03 21:59:20 -0400

    add cart totals component

--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { ProductConsumer } from "../../context";
 
 export default function CartTotals() {
-  return <div>aum namah shivaya from cart totals</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <ProductConsumer>
+          {value => {
+            const { clearCart, cartSubTotal, cartTax, cartTotal } = value;
+            return (
+              <div className="col text-title text-center my-4">
+                <button
+                  className="btn btn-outline-danger text-capitalize mb-4"
+                  onClick={clearCart}
+                >
+                  clear cart
+                </button>
+                <h3>subtotal : ${cartSubTotal}</h3>
+                <h3>tax : ${cartTax}</h3>
+                <h3>total : ${cartTotal}</h3>
+              </div>
+            );
+          }}
+        </ProductConsumer>
+      </div>
+    </div>
+  );
 }
