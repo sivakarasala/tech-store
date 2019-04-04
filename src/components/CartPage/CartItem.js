5b0043b0d35e2df3b1b5625f3546a3b73b7bc59b
shambho <Sivakrishna.Karasala@eku.edu>
2019-04-03 22:33:53 -0400

    add cart item component

--- src/components/CartPage/CartItem.js
+++ src/components/CartPage/CartItem.js
@@ -1,5 +1,64 @@
 import React from "react";
-
-export default function CartItem() {
-  return <div>aum namah shivaya from cart item</div>;
+import {
+  FaTrash,
+  FaChevronCircleUp,
+  FaChevronCircleDown
+} from "react-icons/fa";
+export default function CartItem({
+  cartItem,
+  increment,
+  decrement,
+  removeItem
+}) {
+  const { id, title, price, count, total, image } = cartItem;
+  return (
+    <div className="row mt-5 mt-lg-0 text-capitalize text-center align-items-center">
+      {/* image */}
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <img src={image} width="60" className="img-fluid" alt="product" />
+      </div>
+      {/*end of image */}
+      {/* title */}
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none">product :</span>
+        {title}
+      </div>
+      {/*end of title */}
+      {/* price */}
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none">price : </span>${price}
+      </div>
+      {/*end of price */}
+      {/* count controls */}
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0 ">
+        <div className="d-flex justify-content-center">
+          <div>
+            <FaChevronCircleDown
+              className="cart-icon text-primary"
+              onClick={() => decrement(id)}
+            />
+            <span className="text-title text-muted mx-3">{count}</span>
+            <FaChevronCircleUp
+              className="cart-icon text-primary"
+              onClick={() => increment(id)}
+            />
+          </div>
+        </div>
+      </div>
+      {/*end of count controls */}
+      {/* remove */}
+      <div className="col-10 mx-auto col-lg-2">
+        <FaTrash
+          className="cart-icon text-danger"
+          onClick={() => removeItem(id)}
+        />
+      </div>
+      {/*end of remove */}
+      {/* total */}
+      <div className="col-10 mx-auto col-lg-2">
+        <strong className="text-muted">item total: ${total}</strong>
+      </div>
+      {/*end of total */}
+    </div>
+  );
 }
