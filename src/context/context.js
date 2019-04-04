1fb7bbeb6f4d6f17ac6013024010adb843b58829
shambho <Sivakrishna.Karasala@eku.edu>
2019-04-03 22:56:56 -0400

    add clear cart and decrement methods

--- src/context/context.js
+++ src/context/context.js
@@ -185,7 +185,27 @@ class ProductProvider extends Component {
   };
   // decrement
   decrement = id => {
-    console.log(id);
+    let tempCart = [...this.state.cart];
+    const cartItem = tempCart.find(item => item.id === id);
+
+    cartItem.count = cartItem.count - 1;
+    if (cartItem.count === 0) {
+      this.removeItem(id);
+    } else {
+      cartItem.total = cartItem.count * cartItem.price;
+      cartItem.total = parseFloat(cartItem.total.toFixed(2));
+      this.setState(
+        () => {
+          return {
+            cart: [...tempCart]
+          };
+        },
+        () => {
+          this.addTotals();
+          this.syncStorage();
+        }
+      );
+    }
   };
   // removeItem
   removeItem = id => {
@@ -198,7 +218,10 @@ class ProductProvider extends Component {
   };
 
   clearCart = () => {
-    console.log("aum namah shivaya cleared cart");
+    this.setState({ cart: [] }, () => {
+      this.addTotals();
+      this.syncStorage();
+    });
   };
 
   render() {
