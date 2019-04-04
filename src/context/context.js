f2ac2bff73e5cf8490b0d274c6d9f755e8f1631d
shambho <Sivakrishna.Karasala@eku.edu>
2019-04-03 22:48:24 -0400

    add removeItem logic

--- src/context/context.js
+++ src/context/context.js
@@ -189,7 +189,12 @@ class ProductProvider extends Component {
   };
   // removeItem
   removeItem = id => {
-    console.log(id);
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter(item => item.id !== id);
+    this.setState({ cart: [...tempCart] }, () => {
+      this.addTotals();
+      this.syncStorage();
+    });
   };
 
   clearCart = () => {
