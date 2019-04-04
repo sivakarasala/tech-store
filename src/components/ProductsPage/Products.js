14f86a1a3a6d50574cd42a6e332335cc670a1c79
shambho <Sivakrishna.Karasala@eku.edu>
2019-04-03 23:20:32 -0400

    product filter setup - 2

--- src/components/ProductsPage/Products.js
+++ src/components/ProductsPage/Products.js
@@ -14,12 +14,27 @@ export default function Products() {
             <div className="container">
               {/* title */}
               <Title center title="our products" />
+              {/* product filter */}
               <ProductFilter />
+              {/* total count */}
+              <div className="row">
+                <div className="col-10 mx-auto">
+                  <h6 className="text-title">
+                    total products : {filteredProducts.length}
+                  </h6>
+                </div>
+              </div>
               {/* products */}
               <div className="row py-5">
-                {filteredProducts.map(product => {
-                  return <Product key={product.id} product={product} />;
-                })}
+                {filteredProducts.length === 0 ? (
+                  <div className="col text-title text-center">
+                    sorry, no items matched your search
+                  </div>
+                ) : (
+                  filteredProducts.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  })
+                )}
               </div>
             </div>
           </section>
