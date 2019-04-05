448a5e40855e8eda41696eb948a588a368cb604d
shambho <Sivakrishna.Karasala@eku.edu>
2019-04-04 20:00:53 -0400

    add contentful for fetching items

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { linkData } from "./linkData";
 import { socialData } from "./socialData";
-import { items } from "./productData";
+// import { items } from "./productData";
+import { client } from "./contentful";
+
 const ProductContext = React.createContext();
 //Provider
 //Consumer
@@ -32,7 +34,13 @@ class ProductProvider extends Component {
   componentDidMount() {
     // from contentful items
 
-    this.setProducts(items);
+    // this.setProducts(items);
+    client
+      .getEntries({
+        content_type: "techStoreProducts"
+      })
+      .then(response => this.setProducts(response.items))
+      .catch(err => console.log(err));
   }
 
   // set products
